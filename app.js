a3e01a2042d5f250874f7c992a4f539c48b2255b
samantha-aguirrec <123768420+samantha-aguirrec@users.noreply.github.com>
2023-02-10 14:01:48 -0500

    images, quiz

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardAdj = [
+    {
+      name: "c",
+      img: "images/c.png"
+    }
+  ];
+
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+});
