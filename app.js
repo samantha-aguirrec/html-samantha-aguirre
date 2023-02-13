5799a7c6237e54d726eac824c912e7f80b913481
Isaangel <123569844+Isaangel@users.noreply.github.com>
2023-02-13 12:37:42 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const cardAdj = [
-    {
-      name: "c",
-      img: "images/c.png"
-    }
+    { name: "ardilla (1)", img: "images/ardilla (1).jpg" },
+    { name: "gato (1)", img: "gato (1).jpg" },
+    { name: "jirafa (1)", img: "images/jirafa (1).jpg" },
+    { name: "nutria (1)", img: "images/nutria (1).jpg" },
+    { name: "perro (1)", img: "images/perro (1).jpg" },
+    { name: "pigs (1)", img: "images/pigs (1).jpg" },
+    { name: "ardilla (1)", img: "images/ardilla (1).jpg" },
+    { name: "gato (1)", img: "gato (1).jpg" },
+    { name: "jirafa (1)", img: "images/jirafa (1).jpg" },
+    { name: "nutria (1)", img: "images/nutria (1).jpg" },
+    { name: "perro (1)", img: "images/perro (1).jpg" },
+    { name: "pigs (1)", img: "images/pigs (1).jpg" }
   ];
 
   const cuadricula = document.querySelector(".cuadricula");
