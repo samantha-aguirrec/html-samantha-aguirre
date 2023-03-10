c3f0bd04efb340eecde04809f225400a4b93a039
samantha-aguirrec <123768420+samantha-aguirrec@users.noreply.github.com>
2023-03-10 13:58:52 -0500

    ....

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const cardAdj = [
-    {
-      name: "c",
-      img: "images/c.png"
-    }
+    { name: "ardilla(1)", img: "images/ardilla(1).jpg" },
+    { name: "gato(1)", img: "images/gato(1).jpg" },
+    { name: "jirafa(1)", img: "images/jirafa(1).jpg" },
+    { name: "nutria(1)", img: "images/nutria(1).jpg" },
+    { name: "perro(1)", img: "images/perro(1).jpg" },
+    { name: "pigs(1)", img: "images/pigs(1).jpg" },
+    { name: "ardilla(1)", img: "images/ardilla(1).jpg" },
+    { name: "gato(1)", img: "images/gato(1).jpg" },
+    { name: "jirafa(1)", img: "images/jirafa(1).jpg" },
+    { name: "nutria(1)", img: "images/nutria(1).jpg" },
+    { name: "perro(1)", img: "images/perro(1).jpg" },
+    { name: "pigs(1)", img: "images/pigs(1).jpg" }
   ];
 
   const cuadricula = document.querySelector(".cuadricula");
@@ -25,4 +33,15 @@ document.addEventListener("DOMContentLoaded", () => {
       cuadricula.appendChild(carta);
     }
   }
+
+  function voltearCarta() {
+    var cardId = this.getAttribute("data-id");
+    cartasEscogidas.push(cardAdj[cardId].name);
+    cartasEscogidasId.push(cardId);
+    this.setAttribute("src", cardAdj[cardId].img);
+    if (cartasEscogidas.length === 2) {
+      setTimeout(verificarPareja, 1000);
+    }
+  }
+  crearTablero();
 });
