b48e7c345167b6f4140649703e465d8aef849599
samantha-aguirrec <123768420+samantha-aguirrec@users.noreply.github.com>
2023-03-13 12:30:33 -0500

    miau

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const cardAdj = [
-    { name: "ardilla(1)", img: "images/ardilla(1).jpg" },
-    { name: "gato(1)", img: "images/gato(1).jpg" },
-    { name: "jirafa(1)", img: "images/jirafa(1).jpg" },
-    { name: "nutria(1)", img: "images/nutria(1).jpg" },
-    { name: "perro(1)", img: "images/perro(1).jpg" },
-    { name: "pigs(1)", img: "images/pigs(1).jpg" },
-    { name: "ardilla(1)", img: "images/ardilla(1).jpg" },
-    { name: "gato(1)", img: "images/gato(1).jpg" },
-    { name: "jirafa(1)", img: "images/jirafa(1).jpg" },
-    { name: "nutria(1)", img: "images/nutria(1).jpg" },
-    { name: "perro(1)", img: "images/perro(1).jpg" },
-    { name: "pigs(1)", img: "images/pigs(1).jpg" }
+    { name: "ardilla(1)", img: "images/ardilla (1).jpg" },
+    { name: "gato(1)", img: "images/gato (1).jpg" },
+    { name: "jirafa(1)", img: "images/jirafa (1).jpg" },
+    { name: "nutria(1)", img: "images/nutria (1).jpg" },
+    { name: "perro(1)", img: "images/perro (1).jpg" },
+    { name: "pigs(1)", img: "images/pigs (1).jpg" },
+    { name: "ardilla(1)", img: "images/ardilla (1).jpg" },
+    { name: "gato(1)", img: "images/gato (1).jpg" },
+    { name: "jirafa(1)", img: "images/jirafa (1).jpg" },
+    { name: "nutria(1)", img: "images/nutria (1).jpg" },
+    { name: "perro(1)", img: "images/perro (1).jpg" },
+    { name: "pigs(1)", img: "images/pigs (1).jpg" }
   ];
 
   const cuadricula = document.querySelector(".cuadricula");
